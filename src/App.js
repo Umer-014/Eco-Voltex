d38dbff2ecac7b5f2df8be1ed761509b1055d892
Umer-014 <umer.riaz2810@gmail.com>
2025-03-12 06:07:06 +0500

    vercal update

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from "react";
 import Stack from "./pages/Stack/Stack";
 import { SpeedInsights } from "@vercel/speed-insights/react";
-
+import { Analytics } from "@vercel/analytics/react"
 
 const App = () => {
   return (
     <>
+    <Analytics/>
     <SpeedInsights /> 
     <Stack />
     </>
