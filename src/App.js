bb463304f316ea22ff1b007dae36180c3f7f966c
Umer-014 <umer.riaz2810@gmail.com>
2025-08-17 22:23:33 +0500

    add the ga4 in app.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
-import React from "react";
+import React, { useEffect } from "react";
+import ReactGA from "react-ga4";
 import Stack from "./pages/Stack/Stack";
 import { SpeedInsights } from "@vercel/speed-insights/react";
 import { Analytics } from "@vercel/analytics/react"
 
 const App = () => {
+    useEffect(() => {
+    ReactGA.initialize("G-XXXXXXXXXX"); // replace with your ID
+    ReactGA.send("pageview"); // log first page load
+  }, []);
+  
   return (
     <>
     <Analytics/>
