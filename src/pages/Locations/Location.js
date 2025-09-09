9f4be4457fa4e52b557d558d7747b1f0859e3056
Umer-014 <umer.riaz2810@gmail.com>
2025-09-10 01:32:18 +0500

    set the initial setup of location page befre review

--- src/pages/Locations/Location.js
+++ src/pages/Locations/Location.js
@@ -1,21 +1,161 @@
-import React from "react";
+import React, { useState } from "react";
 import Header from "../../components/Header/Header";
 import Footer from "../../components/Footer/Footer";
 import "./Location.css";
 
 const Location = () => {
+  const regions = {
+    "Central London": [
+      "Aldgate",
+      "Bank",
+      "Barbican",
+      "Belgravia",
+      "Charing Cross",
+      "Covent Garden",
+      "Euston",
+      "Farringdon",
+      "Holborn",
+      "Kings Cross",
+      "Soho",
+      "Strand",
+      "Westminster",
+      "Mayfair",
+      "Paddington",
+      "Kensington",
+      "Knightsbridge",
+      "Notting Hill",
+      "Bayswater",
+      "Holland Park",
+    ],
+    "North London": [
+      "Archway",
+      "Arsenal",
+      "Barnet",
+      "Camden",
+      "Colindale",
+      "Edgware",
+      "Enfield",
+      "Finchley",
+      "Golders Green",
+      "Hampstead",
+      "Harrow",
+      "Hendon",
+      "Holloway",
+      "Highbury",
+      "Highgate",
+      "Islington",
+      "Kentish Town",
+      "Kilburn",
+      "Muswell Hill",
+      "Park Royal",
+      "Seven Sisters",
+      "Tottenham",
+      "Wembley",
+      "Wood Green",
+    ],
+    "East London": [
+      "Barking",
+      "Bow",
+      "Bethnal Green",
+      "Clapton",
+      "Canary Wharf",
+      "Dagenham",
+      "Docklands",
+      "East Ham",
+      "Hackney",
+      "Ilford",
+      "Leyton",
+      "Limehouse",
+      "Manor Park",
+      "Newham",
+      "Old Street",
+      "Shoreditch",
+      "Stratford",
+      "Tower Hamlets",
+      "Tower Hill",
+      "Walthamstow",
+      "West Ham",
+      "Whitechapel",
+    ],
+    "South West London": [
+      "Balham",
+      "Barnes",
+      "Battersea",
+      "Chelsea",
+      "Clapham",
+      "Earl’s Court",
+      "Fulham",
+      "Pimlico",
+      "Putney",
+      "Victoria",
+      "Wandsworth",
+      "Wimbledon",
+      "Tooting",
+    ],
+    "South East London": [
+      "Bermondsey",
+      "Blackheath",
+      "Bromley",
+      "Camberwell",
+      "Catford",
+      "Charlton",
+      "Crystal Palace",
+      "Deptford",
+      "Dulwich",
+      "Elephant & Castle",
+      "Greenwich",
+      "Lewisham",
+      "London Bridge",
+      "New Cross",
+      "Plumstead",
+      "Southwark",
+      "Sidcup",
+      "Surrey Quays",
+      "Waterloo",
+      "Woolwich",
+    ],
+    "West London": [
+      "Acton",
+      "Chiswick",
+      "Ealing",
+      "Hammersmith",
+      "Shepherd’s Bush",
+      "Maida Vale",
+      "Holland Park",
+      "Bayswater",
+    ],
+    "Greater London & Surroundings": [
+      "Croydon",
+      "Hayes",
+      "Heathrow",
+      "Hounslow",
+      "Isleworth",
+      "Morden",
+      "Richmond",
+      "Ruislip",
+      "Slough",
+      "Twickenham",
+      "Watford",
+    ],
+  };
+
+  const [selectedRegion, setSelectedRegion] = useState("");
+
   return (
     <>
       <Header />
       <div className="location-container">
+        {/* Location Header Section */}
         <section className="location-header">
           <h1 className="location-header-title">Our Locations</h1>
           <p className="location-header-description">
             Your Local Electrical Experts Across London & Nearby Regions.
           </p>
         </section>
-
+        {/* Location Details Section */}
+        <h2 className="section-heading">Our Location & Coverage</h2>
         <section className="location-details">
+          
           <div className="text-paragraph">
             <p>
               At{" "}
@@ -47,9 +187,55 @@ const Location = () => {
             ></iframe>
           </div>
         </section>
+        {/* Location name plus filter */}
+        <h2 className="section-heading">Find Us in Your Area</h2>
+        <section className="location-name">
+          
+          {/* Filter (desktop right, mobile top) */}
+          <div className="location-filter">
+            <label htmlFor="regionSelect" className="filter-label">
+              Select Region
+            </label>
+            <select
+              id="regionSelect"
+              className="filter-select"
+              value={selectedRegion}
+              onChange={(e) => setSelectedRegion(e.target.value)}
+            >
+              <option value="">-- Choose a region --</option>
+              {Object.keys(regions).map((region) => (
+                <option key={region} value={region}>
+                  {region}
+                </option>
+              ))}
+            </select>
+          </div>
+
+          {/* Content area */}
+          <div className="location-list">
+            <p className="intro">
+              At <strong>Eco Voltex</strong>, we cover London and its
+              surrounding areas. Choose a region to see the specific areas we
+              serve.
+            </p>
+
+            {/* Only show areas when region is selected */}
+            {selectedRegion && (
+              <>
+                <h3 className="region-title">{selectedRegion}</h3>
+                <ul className="areas">
+                  {regions[selectedRegion].map((area, i) => (
+                    <li key={i}>{area}</li>
+                  ))}
+                </ul>
+              </>
+            )}
+          </div>
+        </section>
       </div>
       <Footer />
     </>
   );
 };
+
 export default Location;
