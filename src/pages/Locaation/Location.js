77aab3f1a2263367f24d34a227c4859f2308ee92
Umer-014 <umer.riaz2810@gmail.com>
2025-09-09 13:37:04 +0500

    js and css of Location page

---
+++ src/pages/Locaation/Location.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Header from "../../components/Header/Header";
+import Footer from "../../components/Footer/Footer";
+import "./Location.css";
+const Location = () => {
+  return (
+    <>
+      <Header />
+      <div style={{ minHeight: "80vh", display: "flex", justifyContent: "center", alignItems: "center" }}>
+        <h1>Location Page</h1>
+      </div>
+      <Footer />
+    </>
+  );
+}
