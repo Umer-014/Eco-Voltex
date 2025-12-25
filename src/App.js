b88bf3d3bcbaa08bad87ccb63cc9cacc372dfefc
Umer-014 <umer.riaz2810@gmail.com>
2025-12-26 01:05:15 +0500

    apply the logic of snow and fire work for appearing on all pages..

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import Stack from "./pages/Stack/Stack";
 import { SpeedInsights } from "@vercel/speed-insights/react";
 import { Analytics } from "@vercel/analytics/react"
+import { useEffect } from "react";
+import { loadSeasonalEffects, cleanupSeasonalEffects } from "./pages/Pricing Layout/seasonalEffects";
 
 const App = () => {
-
+useEffect(() => {
+  loadSeasonalEffects();
+  return () => cleanupSeasonalEffects();
+}, []);
 
   return (
     <>
